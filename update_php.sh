c4aa8a23b761982ed2c835668ad92821fa06e2c4
Marten Tacoma <Eupeodes@users.noreply.github.com>
2019-02-13 08:30:45 +0100

    Create update_php.sh
    simple script to update installed php to new version

---
+++ update_php.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+oldVersion=7.0
+newVersion=7.3
+
+phplist2=$(apt list --installed | grep php$oldVersion | awk -F '[/:]' '{print $1}')
+sudo apt install ${phplist//$oldVersion/$newVersion}
+
+sudo apt remove php$oldVersion*
