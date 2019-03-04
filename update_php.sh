ed0377cc226f76a50c6e9c713501e90af5089cb3
Marten Tacoma <Eupeodes@users.noreply.github.com>
2019-03-04 07:59:53 +0100

    Update update_php.sh

--- update_php.sh
+++ update_php.sh
@@ -3,7 +3,7 @@
 oldVersion=7.0
 newVersion=7.3
 
-phplist2=$(apt list --installed | grep php$oldVersion | awk -F '[/:]' '{print $1}')
+phplist=$(apt list --installed | grep php$oldVersion | awk -F '[/:]' '{print $1}')
 sudo apt install ${phplist//$oldVersion/$newVersion}
 
 sudo apt remove php$oldVersion*
