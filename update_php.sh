ff5e36614dfb32c1c06d655a44214f3939544f87
Marten Tacoma <Eupeodes@users.noreply.github.com>
2022-01-25 12:51:47 +0100

    Update update_php.sh

--- update_php.sh
+++ update_php.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-oldVersion=7.0
-newVersion=7.3
+read -p "Old version: " oldVersion
+read -p "New version: " newVersion
 
 phplist=$(apt list --installed | grep php$oldVersion | awk -F '[/:]' '{print $1}')
 sudo apt install ${phplist//$oldVersion/$newVersion}
 
-sudo apt remove php$oldVersion*
+read -p "Do you want to remove PHP $oldVersion? [y/N]" -n 1 -r
+if [[ $REPLY =~ ^[Yy]$ ]]
+  then
+    sudo apt remove php$oldVersion*
+fi
